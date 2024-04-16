703953eb3213d9afc6d3cca030e6bf094405f720
qminhminh <hqminh050503@gmail.com>
2024-04-16 10:26:50 +0700

    phan 5: catgorylist and catogr controller

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:foodappflutter/common/custom_appbar.dart';
 import 'package:foodappflutter/common/custom_container.dart';
 import 'package:foodappflutter/constants/constants.dart';
+import 'package:foodappflutter/views/home/widgets/category_list.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -14,7 +17,10 @@ class HomePage extends StatelessWidget {
       appBar: PreferredSize(
           preferredSize: Size.fromHeight(130.h), child: const CustomAppBar()),
       body: SafeArea(
-        child: CustomContainer(containerContent: Container()),
+        child: CustomContainer(
+            containerContent: Column(
+          children: [CategoryList()],
+        )),
       ),
     );
   }

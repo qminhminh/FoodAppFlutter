edc14b09617876d3253960c37c453ba39f05c834
qminhminh <hqminh050503@gmail.com>
2024-04-17 15:23:14 +0700

    phan 18.3: hôkfood and get food

--- lib/views/home/all_nearby_restaurants.dart
+++ lib/views/home/all_nearby_restaurants.dart
@@ -1,38 +1,50 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:foodappflutter/common/app_style.dart';
 import 'package:foodappflutter/common/back_ground_container.dart';
 import 'package:foodappflutter/common/reusable_text.dart';
+import 'package:foodappflutter/common/shimmers/foodlist_shimmer.dart';
 import 'package:foodappflutter/constants/constants.dart';
 import 'package:foodappflutter/constants/uidata.dart';
+import 'package:foodappflutter/hooks/fetch_all_restaurants.dart';
+import 'package:foodappflutter/models/restaurants_model.dart';
 import 'package:foodappflutter/views/home/widgets/restuarant_tile.dart';
 
-class AllNearbyRestaurants extends StatelessWidget {
+class AllNearbyRestaurants extends HookWidget {
   const AllNearbyRestaurants({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final hookResults = useFetchAllRestaurants("41007428");
+    List<RestaurantsModel>? restaurants = hookResults.data;
+    final isLoading = hookResults.isLoading;
     return Scaffold(
+      backgroundColor: kSecondary,
       appBar: AppBar(
-        elevation: 0.3,
+        elevation: 0,
         backgroundColor: kSecondary,
         title: ReusableText(
             text: "Nearby Restaurants",
-            style: appStyle(13, kGray, FontWeight.w600)),
+            style: appStyle(13, kLightWhite, FontWeight.w600)),
       ),
       body: BackGroundContainer(
         color: Colors.white,
-        child: Padding(
-          padding: EdgeInsets.all(12.h),
-          child: ListView(
-            children: List.generate(restaurants.length, (i) {
-              var restaurant = restaurants[i];
-              return RestaurantTile(
-                restaurant: restaurant,
-              );
-            }),
-          ),
-        ),
+        child: isLoading
+            ? const FoodsListShimmer()
+            : Padding(
+                padding: EdgeInsets.all(12.h),
+                child: ListView(
+                  children: List.generate(restaurants!.length, (i) {
+                    RestaurantsModel restaurant = restaurants[i];
+                    return RestaurantTile(
+                      restaurant: restaurant,
+                    );
+                  }),
+                ),
+              ),
       ),
     );
   }
--- lib/views/home/widgets/food_tile.dart
+++ lib/views/home/widgets/food_tile.dart
@@ -7,11 +7,12 @@ import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:foodappflutter/common/app_style.dart';
 import 'package:foodappflutter/common/reusable_text.dart';
 import 'package:foodappflutter/constants/constants.dart';
+import 'package:foodappflutter/models/foods_model.dart';
 
 class FoodTile extends StatelessWidget {
   FoodTile({super.key, required this.food});
 
-  final dynamic food;
+  final FoodsModel food;
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +40,7 @@ class FoodTile extends StatelessWidget {
                             width: 70.w,
                             height: 70.h,
                             child: Image.network(
-                              food["imageUrl"],
+                              food.imageUrl[0],
                               fit: BoxFit.cover,
                             ),
                           ),
@@ -70,11 +71,11 @@ class FoodTile extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         ReusableText(
-                          text: food['title'],
+                          text: food.title,
                           style: appStyle(11, kDark, FontWeight.w400),
                         ),
                         ReusableText(
-                          text: "Delivery time: ${food['time']}",
+                          text: "Delivery time: ${food.time}",
                           style: appStyle(11, kGray, FontWeight.w400),
                         ),
                         SizedBox(
@@ -82,9 +83,9 @@ class FoodTile extends StatelessWidget {
                           height: 15.h,
                           child: ListView.builder(
                               scrollDirection: Axis.horizontal,
-                              itemCount: food['additives'].length,
+                              itemCount: food.additives.length,
                               itemBuilder: (context, i) {
-                                var additive = food['additives'][i];
+                                Additive additive = food.additives[i];
                                 return Container(
                                   margin: EdgeInsets.only(right: 5.w),
                                   decoration: BoxDecoration(
@@ -97,7 +98,7 @@ class FoodTile extends StatelessWidget {
                                     child: Padding(
                                       padding: EdgeInsets.all(2.h),
                                       child: ReusableText(
-                                          text: additive['title'],
+                                          text: additive.title,
                                           style: appStyle(
                                               8, kGray, FontWeight.w400)),
                                     ),
@@ -121,7 +122,7 @@ class FoodTile extends StatelessWidget {
                     color: kPrimary, borderRadius: BorderRadius.circular(10.r)),
                 child: Center(
                   child: ReusableText(
-                      text: "\$ ${food['price'].toStringAsFixed(2)}",
+                      text: "\$ ${food.price.toStringAsFixed(2)}",
                       style: appStyle(12, kLightWhite, FontWeight.bold)),
                 ),
               ),

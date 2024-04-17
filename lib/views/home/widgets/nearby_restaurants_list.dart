3f1fbdd99bb2a3e1c0ad486ed496d715fe375df0
qminhminh <hqminh050503@gmail.com>
2024-04-17 14:18:25 +0700

    phan 18.2: hoook restaurant and get all restaurants

--- lib/views/home/widgets/nearby_restaurants_list.dart
+++ lib/views/home/widgets/nearby_restaurants_list.dart
@@ -1,29 +1,41 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodappflutter/common/shimmers/nearby_shimmer.dart';
 import 'package:foodappflutter/constants/uidata.dart';
+import 'package:foodappflutter/hooks/fetch_restaurants.dart';
+import 'package:foodappflutter/models/restaurants_model.dart';
 import 'package:foodappflutter/views/home/widgets/restaurant_widget.dart';
 
-class NearbyRestaurants extends StatelessWidget {
+class NearbyRestaurants extends HookWidget {
   const NearbyRestaurants({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 190.h,
-      padding: EdgeInsets.only(left: 12.w, top: 10.h),
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: List.generate(restaurants.length, (i) {
-          var restaurant = restaurants[i];
-          return RestaurantWidget(
-            image: restaurant['imageUrl'] as String,
-            logo: restaurant['logoUrl'] as String,
-            title: restaurant['title'] as String,
-            time: restaurant['time'] as String,
-            rating: restaurant['ratingCount'] as String,
+    final hookResults = useFetchRestaurants("41007428");
+    List<RestaurantsModel>? restaurants = hookResults.data;
+    final isLoading = hookResults.isLoading;
+
+    return isLoading
+        ? const NearbyShimmer()
+        : Container(
+            height: 190.h,
+            padding: EdgeInsets.only(left: 12.w, top: 10.h),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: List.generate(restaurants!.length, (i) {
+                RestaurantsModel restaurant = restaurants[i];
+                return RestaurantWidget(
+                  image: restaurant.imageUrl,
+                  logo: restaurant.logoUrl,
+                  title: restaurant.title,
+                  time: restaurant.time,
+                  rating: "7457",
+                );
+              }),
+            ),
           );
-        }),
-      ),
-    );
   }
 }
